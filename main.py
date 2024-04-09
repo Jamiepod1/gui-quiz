8a4625db5090aebb729e45261bbeaa4c1ac05531
Jamiepod1 <jamieplumy@gmail.com>
2024-04-09 15:44:21 +0100

    run from main.py, added argv inputs, '-c' to run in console mode, '-g' to run in GUI mode

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from quiz import Quiz
 from gui import QuizGui
+import sys
 
-quiz_gui = QuizGui()
+def main():
+    if len(sys.argv) == 2:
+        if sys.argv[1] == "-c":
+            quiz = Quiz()
+            quiz.start_quiz_console()
+        elif sys.argv[1] == "-g":
+            quiz_gui = QuizGui()
+        else:
+            print("Incorrect argument:\n'c' for console\n'g' for GUI")
+    else:
+        print("Required exactly 2 arguments")
 
+
+main()
