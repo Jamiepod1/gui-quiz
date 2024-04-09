e3b0c1dd1dd17e8366dc47a34ca5a11066d2e31e
Jamiepod1 <jamieplumy@gmail.com>
2024-04-09 14:14:42 +0100

    refactor get_questions, no longer returns, asignes value to self.questions

--- quiz.py
+++ quiz.py
@@ -4,21 +4,21 @@ class Quiz():
     def __init__(self) -> None:
         self.score = 0
         self.url = "https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=boolean"
-        self.questions = self.get_questions()
+        self.questions = {}
+        self.get_questions()
 
 
-    def get_questions(self) -> dict:
+    def get_questions(self):
         try:
             response = requests.get(url=self.url)
             response.raise_for_status()
         except HTTPError:
-            return {}
+            self.questions = {}
         else:
             questions_json = response.json()["results"]
-            questions = {}
             for entry in questions_json:
-                questions[entry["question"]] = entry["correct_answer"]
-            return questions
+                self.questions[entry["question"]] = entry["correct_answer"]
+
         
     
 
