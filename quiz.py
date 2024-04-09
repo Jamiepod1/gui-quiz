4ce825c346c5fa5bea740551b83c96cd268a09ee
Jamiepod1 <jamieplumy@gmail.com>
2024-04-09 14:28:37 +0100

    added __str__ method

--- quiz.py
+++ quiz.py
@@ -1,26 +1,42 @@
 import requests
 
+
 class Quiz():
+
+    
     def __init__(self) -> None:
         self.score = 0
         self.url = "https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=boolean"
-        self.questions = {}
+        self.questions = []
         self.get_questions()
 
+
+    def __str__(self) -> str:
+        if self.questions == []:
+            return "Question list empty"
+        else:
+            output = ""
+            for question in self.questions:
+                output += f"{question["question"]}: {question["answer"]}\n"
+            return output
+            
 
     def get_questions(self):
         try:
             response = requests.get(url=self.url)
             response.raise_for_status()
         except HTTPError:
-            self.questions = {}
+            self.questions = []
         else:
             questions_json = response.json()["results"]
             for entry in questions_json:
-                self.questions[entry["question"]] = entry["correct_answer"]
+                question = {"question": entry["question"], "answer": entry["correct_answer"]}
+                self.questions.append(question)
 
         
-    
+    def ask_question(self):
+        pass
+
 
 quiz = Quiz()
-print(quiz.questions)
+print(quiz)
